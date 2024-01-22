b38e8275823ebf2ae41f096f2cb297f10a0d335b
IsmaelMiranda <ismagamer92ok@gmail.com>
2024-01-22 20:59:44 -0300

    agrego codigo en  app.js

--- app.js
+++ app.js
@@ -24,3 +24,5 @@ app.use(express.static('public'))
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => console.log(`Server up on port: http://localhost:${PORT}`))
+
+app.use(express.static('public')) 
